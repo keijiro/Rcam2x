def0a67d4f09ea7c1d3b73d20eec04cfad39d21f
Keijiro Takahashi <keijiro@gmail.com>
2022-12-06 14:35:56 +0900

    Camera parameter calculation from projection matrix

--- RcamVisualizer/Assets/Script/Camera/ProjectionMatrixLinker.cs
+++ RcamVisualizer/Assets/Script/Camera/ProjectionMatrixLinker.cs
@@ -10,7 +10,19 @@ sealed class ProjectionMatrixLinker : MonoBehaviour
       => _camera = GetComponent<Camera>();
 
     void Update()
-      => _camera.projectionMatrix = Singletons.Receiver.ProjectionMatrix;
+    {
+        // Calculate the camera parameters from the projection matrix.
+        // This doesn't take any effect but gives better compatibility with
+        // component depending on these parameters.
+        var m = Singletons.Receiver.ProjectionMatrix;
+        var (h, z, w) = (m[1, 1], m[2, 2], m[2, 3]);
+        _camera.nearClipPlane = w / (z - 1);
+        _camera.farClipPlane = w / (z + 1);
+        _camera.fieldOfView = Mathf.Rad2Deg * Mathf.Atan(1 / h) * 2;
+
+        // Overwrite the projection matrix.
+        _camera.projectionMatrix = Singletons.Receiver.ProjectionMatrix;
+    }
 }
 
 } // namespace Rcam2
